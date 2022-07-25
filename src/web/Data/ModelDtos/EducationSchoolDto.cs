51470ec54ade896161370cdbb4a2eee4f12e72be
komisar_22 <komisar_22@outlook.com>
2022-07-25 11:06:15 +0300

    added new feature for education school and professional technical

---
+++ src/web/Data/ModelDtos/EducationSchoolDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace web.Data.ModelDtos
+{
+    public class EducationSchoolDto
+    {
+        public int Id { get; set; }       
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }        
+        public string StatusSchoolCertificate { get; set; }
+        public string NumberOfClasses { get; set; }
+        public string NameSchool { get; set; }
+        public string SeriaSchoolCertificate { get; set; }
+        public int NumberSchoolCertificate { get; set; }
+           
+    }
+
+    public class EducationProfessionalTechnicalSchoolDto
+    {
+        public int Id { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string StatusProfessionalTechnicalSchoolCertificate { get; set; }     
+        public string NameProfessionalTechnicalSchool { get; set; }
+        public string SeriaProfessionalTechnicalSchoolCertificate { get; set; }
+        public int NumberProfessionalTechnicalSchoolCertificate { get; set; }
+        public string SpecialtyProfessionalTechnicalSchool { get; set; }
+    }
+}
