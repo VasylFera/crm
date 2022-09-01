aa2f6f2f15e2cea85a5dbac3cf4201a6e656e44e
komisar_22 <komisar_22@outlook.com>
2022-09-01 15:57:29 +0300

    fixed search logic

--- src/web/Data/Adapter/PersonalDataAdapter.cs
+++ src/web/Data/Adapter/PersonalDataAdapter.cs
@@ -125,39 +125,6 @@ namespace web.Data.Adapter
                 DataBaseHelper.SafeSqlString(model.ServedDescription));
                 DataBaseHelper.RunSql(sql);
             }           
-        }
-
-        public static IEnumerable<PersonalDataSearchDto> GetPersonalSearch(PersonalDataSearchDto model)
-        {
-            var result = new List<PersonalDataSearchDto>();
-                      
-            string sql = null;
-            sql = string.Format(@"exec [sp_GetPersonDataSearch] {0},{1},{2},{3},{4}",
-            //DataBaseHelper.RawSafeSqlString(model.Id),
-            DataBaseHelper.SafeSqlString(model.LastName),
-            DataBaseHelper.SafeSqlString(model.FirstName),
-            DataBaseHelper.SafeSqlString(model.FatherName),
-            DataBaseHelper.SafeSqlString(model.IdentityNumber),
-            DataBaseHelper.SafeSqlString(model.ResidentialAddress));
-            var sqlResult = DataBaseHelper.GetSqlResult(sql);
-
-            if (sqlResult.Rows.Count > 0)
-            {
-                foreach (DataRow item in sqlResult.Rows)
-                {
-                    result.Add(new PersonalDataSearchDto
-                    {
-                        Id = DataBaseHelper.GetIntegerValueFromRowByName(item, "Id"),
-                        LastName = DataBaseHelper.GetValueFromRowByName(item, "LastName"),
-                        FirstName = DataBaseHelper.GetValueFromRowByName(item, "FirstName"),
-                        FatherName = DataBaseHelper.GetValueFromRowByName(item, "FatherName"),
-                        IdentityNumber = DataBaseHelper.GetValueFromRowByName(item, "IndividualPersonalNumber"),
-                        ResidentialAddress = DataBaseHelper.GetValueFromRowByName(item, "VillageName")                       
-                    });
-                }
-            }
-
-            return result;
-        }
+        }              
     }
 }
--- src/web/Data/ModelDtos/PersonalDataSearchDto.cs
+++ src/web/Data/ModelDtos/PersonalDataSearchDto.cs
@@ -8,5 +8,6 @@
         public string FatherName { get; set; }
         public string IdentityNumber { get; set; }
         public string ResidentialAddress { get; set; }
+        public string BirthDay { get; set; }
     }
 }
